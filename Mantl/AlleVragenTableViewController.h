08258eb00023b0e2d45e9ba0b8301fa95dbe3025
Sammi Fux <sammi.fux@student.ehb.be>
2014-05-20 13:09:17 +0200

    Update Vragen en antwoorden

---
+++ Mantl/AlleVragenTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AlleVragenTableViewController.h
+//  Mantl
+//
+//  Created by Sammi Fux on 20/05/14.
+//  Copyright (c) 2014 Mantelzorgers Groep 2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlleVragenTableViewController : UITableViewController
+
+@end
