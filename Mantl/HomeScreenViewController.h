1ad8e51eed6da0c08e934deadea9715faa5d913e
Sammi Fux <sammi.fux@student.ehb.be>
2014-05-21 10:50:34 +0200

    New composed project

---
+++ Mantl/HomeScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeScreenViewController.h
+//  Mantl
+//
+//  Created by Sammi Fux on 17/05/14.
+//  Copyright (c) 2014 Mantelzorgers Groep 2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeScreenViewController : UIViewController
+
+@end
