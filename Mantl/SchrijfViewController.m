29971805fe2856a5e370a922a8e0bcda93b1d91c
brentheynsmans <brent.heynsmans@student.ehb.be>
2014-05-21 16:09:09 +0200

    Dagboek data inladen

--- Mantl/SchrijfViewController.m
+++ Mantl/SchrijfViewController.m
@@ -45,6 +45,9 @@
     // Pass the selected object to the new view controller.
 }
 */
+- (IBAction)back:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 
 - (IBAction)opslaan:(id)sender {
 }
