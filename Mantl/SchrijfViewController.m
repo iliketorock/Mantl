3f92b920cfab6209c328b2c110da02fa34f57074
brentheynsmans <brent.heynsmans@student.ehb.be>
2014-05-21 11:01:02 +0200

    brent

---
+++ Mantl/SchrijfViewController.m
@@ -0,0 +1,51 @@
+//
+//  SchrijfViewController.m
+//  Mantl
+//
+//  Created by Brent Heynsmans on 21/05/14.
+//  Copyright (c) 2014 Mantelzorgers Groep 2. All rights reserved.
+//
+
+#import "SchrijfViewController.h"
+
+@interface SchrijfViewController ()
+
+@end
+
+@implementation SchrijfViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)opslaan:(id)sender {
+}
+@end
