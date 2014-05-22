f0a5f957911813352c9263572fad3cbffc6f553c
Gitte Pittillion <gitte.pittillion@hotmail.com>
2014-05-22 13:57:30 +0200

    Op Stap

---
+++ Mantl/plaatsViewController.h
@@ -0,0 +1,19 @@
+//
+//  plaatsViewController.h
+//  Mantl
+//
+//  Created by Gitte Pittillion on 22/05/14.
+//  Copyright (c) 2014 Mantelzorgers Groep 2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Home.h"
+
+@interface plaatsViewController : UIViewController <UITableViewDataSource, UITableViewDelegate, HomeProtocol>
+
+
+@property (weak, nonatomic) IBOutlet UITableView *plaatsTableView;
+
+
+
+@end
