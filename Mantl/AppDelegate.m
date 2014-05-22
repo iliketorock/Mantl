bb22ca07e3e74f7d1970261cd659c587f2968629
Sammi Fux <sammi.fux@student.ehb.be>
2014-05-22 14:22:02 +0200

    Appdelegate tweede witte gelecteerde icoontjes

--- Mantl/AppDelegate.m
+++ Mantl/AppDelegate.m
@@ -34,7 +34,7 @@
     [[UITabBar appearance] setBarTintColor:UIColorFromRGB(0xE06025)];
     [[UITabBarItem appearance] setTitleTextAttributes:@{                                                        NSForegroundColorAttributeName : [UIColor blackColor]} forState:UIControlStateNormal];
     [[UITabBarItem appearance] setTitleTextAttributes:@{                                                        NSForegroundColorAttributeName : [UIColor whiteColor]} forState:UIControlStateSelected];
-    
+    [[UITabBar appearance] setTintColor:[UIColor whiteColor]];
     
     return YES;
 }
@@ -48,7 +48,7 @@
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
@@ -74,11 +74,11 @@
     NSManagedObjectContext *managedObjectContext = self.managedObjectContext;
     if (managedObjectContext != nil) {
         if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
-             // Replace this implementation with code to handle the error appropriately.
-             // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. 
+            // Replace this implementation with code to handle the error appropriately.
+            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
             NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
             abort();
-        } 
+        }
     }
 }
 
@@ -128,7 +128,7 @@
         /*
          Replace this implementation with code to handle the error appropriately.
          
-         abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. 
+         abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
          
          Typical reasons for an error here include:
          * The persistent store is not accessible;
@@ -150,7 +150,7 @@
          */
         NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
         abort();
-    }    
+    }
     
     return _persistentStoreCoordinator;
 }
